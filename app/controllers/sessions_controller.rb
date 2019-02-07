6c0b2b9fc38cd74ab77ffce256fd22c93d33124a
stephanesdr <stadlerstephane@gmail.com>
2019-02-07 18:51:05 +0100

    log in / out ok but problem with comment delete logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,25 @@
 class SessionsController < ApplicationController
 
+  include SessionsHelper
+
   def new
   end
 
-def create
-
-puts "$" * 67
-  puts params
-  # cherche s'il existe un utilisateur en base avec l’e-mail
-  #   user = User.find_by(email: )
-  #
-  # # on vérifie si l'utilisateur existe bien ET si on arrive à l'authentifier (méthode bcrypt) avec le mot de passe
-  # if user && user.authenticate(password_dans_ton_params)
-  #   session[:user_id] = user.id
-  #   # redirige où tu veux, avec un flash ou pas
-  #
-  # else
-  #   flash.now[:danger] = 'Invalid email/password combination'
-  #   render 'new'
-  # end
-  end
+  def create
+    user = User.find_by('email' => params[:email] )
+      if user && user.authenticate(params["password"])
+        session[:user_id] = user.id
+        redirect_to root_path
+        puts "vous etes connecté #{user.first_name}"
+        else
+        flash.now[:danger] = 'Invalid email/password combination'
+        render 'new'
+      end
+    end
 
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path
   end
 
 end
