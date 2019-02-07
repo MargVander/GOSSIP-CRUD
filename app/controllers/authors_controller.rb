6e51ad6bcc75ad0b781a7826c415ac22e837b5ef
stephanesdr <stadlerstephane@gmail.com>
2019-02-07 16:29:39 +0100

    creation user ok .

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,25 @@
 class AuthorsController < ApplicationController
-  
+
+  def index
+
+  end
   def show
     @user = User.find(params[:id])
-   end
+  end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create('first_name' => params[:first_name], 'last_name' => params[:last_name], city_id: 3, 'age' => params[:age], 'description' => params[:description], 'password' => params[:password])
+
+
+      if @user.save
+        flash[:notice] = "Post successfully created"
+        redirect_to root_path
+      else
+        render 'new'
+      end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
   has_many :private_messages
+  has_secure_password
+
 
   # has_many :sent_messages, foreign_key: 'sender_id', class_name: "PrivateMessage"
   # has_many :received_messages, foreign_key: 'recipient_id', class_name: "MultiPm"
