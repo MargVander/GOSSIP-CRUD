25ce608a1bdfbe39ea24d4a5a0c915aba189eea2
stephanesdr <stadlerstephane@gmail.com>
2019-02-07 19:55:16 +0100

    new db seems ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_secure_password
 
 
+
   # has_many :sent_messages, foreign_key: 'sender_id', class_name: "PrivateMessage"
   # has_many :received_messages, foreign_key: 'recipient_id', class_name: "MultiPm"
   # has_many :multi_pms
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,59 +13,52 @@ ActiveRecord::Base.connection.tables.each do |t|
   ActiveRecord::Base.connection.reset_pk_sequence!(t)
 end
 
-# #Create City
-# liste_cp = ["63920", "93208", "02134", "76300", "91300", "06220", "36912", "46990", "12098", "11273", "88501"]
-# i = 0
-# 5.times do
-#   city = City.create!(name: Faker::Nation.capital_city, zip_code: liste_cp[i])
-#   i += 1
-# end
-# i = 0
-#
-# #Create Tags
-# 5.times do
-#   tag = Tag.create!(title: Faker::Dessert.variety)
-# end
-#
-# #Create User
-# 10.times do
-#   user = User.create!(first_name: Faker::DragonBall.character, last_name: Faker::Name.last_name , description: Faker::HowIMetYourMother.quote, email: Faker::Internet.email, age: Faker::Number.between(18, 75), city_id: City.all.ids.sample)
-# end
-#   #création d'un user anonyme
-#   user = User.create!(first_name: "anonymous", last_name: "anonymous" , description: Faker::HowIMetYourMother.quote, email: Faker::Internet.email, age: Faker::Number.between(18, 75), city_id: City.all.ids.sample)
-#
-# #Create Gossip
-# #checke u moins un tag par gossip
-# 12.times do
-#   gossip = Gossip.create!(title: Faker::Lovecraft.word, content: Faker::ChuckNorris.fact, user_id: User.all.ids.sample)
-# end
-#
-# #Create tags by potin
-# 30.times do
-#   tag_by_gossip = TagByGossip.create!(gossip_id: Gossip.all.ids.sample, tag_id: Tag.all.ids.sample)
-# end
-#
-# #Create Private Message
-# 10.times do
-#   privatemessage = PrivateMessage.create!(content: Faker::GreekPhilosophers.quote, sender_id: User.all.ids.sample)
-# end
-#
-# #create join table
-# 10.times do
-#   multi_pm = MultiPm.create!(user_id: User.all.ids.sample, private_message_id: PrivateMessage.all.ids.sample)
-# end
-#
-# 10.times do
-#   Comment.create!(content: Faker::Fallout.quote, user_id: rand(User.first.id..User.last.id), commenteable_id: rand(Gossip.first.id..Gossip.last.id), commenteable_type: "Gossip")
-#   Comment.create!(content: Faker::Fallout.quote, user_id: rand(User.first.id..User.last.id), commenteable_id: rand(Comment.first.id..Comment.last.id), commenteable_type: "Comment")
-# end
-#
-# 15.times do
-#   Like.create!(likeable_id: rand(Gossip.first.id..Gossip.last.id), likeable_type: "Gossip", user_id: rand(User.first.id..User.last.id))
-#   Like.create!(likeable_id: rand(Comment.first.id..Comment.last.id), likeable_type: "Comment", user_id: rand(User.first.id..User.last.id))
-# end
+#Create City
+liste_cp = ["63920", "93208", "02134", "76300", "91300", "06220", "36912", "46990", "12098", "11273", "88501"]
+i = 0
+5.times do
+  city = City.create!(name: Faker::Nation.capital_city, zip_code: liste_cp[i])
+  i += 1
+end
+i = 0
 
-#create comment
-20.times do
-  comments = Comment.create!(content: Faker::Pokemon.move, user_id: rand(User.first.id..User.last.id))
+#Create Tags
+5.times do
+  tag = Tag.create!(title: Faker::Dessert.variety)
+end
+
+#Create User
+10.times do
+  user = User.create!(first_name: Faker::DragonBall.character, last_name: Faker::Name.last_name , description: Faker::HowIMetYourMother.quote, email: Faker::Internet.email, age: Faker::Number.between(18, 75), city_id: City.all.ids.sample, password: Faker::Internet.password(8))
+end
+
+#Create Gossip
+#checke u moins un tag par gossip
+12.times do
+  gossip = Gossip.create!(title: Faker::Lovecraft.word, content: Faker::ChuckNorris.fact, user_id: User.all.ids.sample)
+end
+
+#Create tags by potin
+30.times do
+  tag_by_gossip = TagByGossip.create!(gossip_id: Gossip.all.ids.sample, tag_id: Tag.all.ids.sample)
+end
+
+#Create Private Message
+10.times do
+  privatemessage = PrivateMessage.create!(content: Faker::GreekPhilosophers.quote, sender_id: User.all.ids.sample)
+end
+
+#create join table
+10.times do
+  multi_pm = MultiPm.create!(user_id: User.all.ids.sample, private_message_id: PrivateMessage.all.ids.sample)
+end
+
+10.times do
+  Comment.create!(content: Faker::Fallout.quote, user_id: rand(User.first.id..User.last.id), commenteable_id: rand(Gossip.first.id..Gossip.last.id), commenteable_type: "Gossip")
+  Comment.create!(content: Faker::Fallout.quote, user_id: rand(User.first.id..User.last.id), commenteable_id: rand(Comment.first.id..Comment.last.id), commenteable_type: "Comment")
+end
+
+15.times do
+  Like.create!(likeable_id: rand(Gossip.first.id..Gossip.last.id), likeable_type: "Gossip", user_id: rand(User.first.id..User.last.id))
+  Like.create!(likeable_id: rand(Comment.first.id..Comment.last.id), likeable_type: "Comment", user_id: rand(User.first.id..User.last.id))
 end
