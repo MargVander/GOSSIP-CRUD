d4bd7cdcaa313e5e3f9f30cdd45c4b7e6594a2ca
stephanesdr <stadlerstephane@gmail.com>
2019-02-08 16:57:27 +0100

    petite modif toujours en stand by, reset le prochain commit si bordel

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -32,9 +32,9 @@ class CommentsController < ApplicationController
     # puts "*" *70
     if @comment.update(content: params[:content], commenteable_id: params[:gossip_id], commenteable_type: "Gossip")
       @comment.user = current_user
-      p "^" * 120
+      p "_-" * 120
       p params
-      p "^" * 120
+      p "_-" * 120
       flash[:notice] = "Comment successfully modified"
       flash[:type] = "info"
       redirect_to gossip_path(params[:gossip_id])
@@ -47,8 +47,9 @@ class CommentsController < ApplicationController
 
   def destroy
     @comment = Comment.find(params[:id])
-    @comment.user = current_user
-
+    p "^" * 120
+    p params
+    p "^" * 120
     @comment.destroy
     flash[:notice] = "Post successfully destroy"
     flash[:type] = "info"
